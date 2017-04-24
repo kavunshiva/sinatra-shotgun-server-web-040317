6a797377ec1eddbd3ffc4fde8fb96a87387666fb
kavunshiva <kavunshiva@gmail.com>
2017-04-24 17:34:51 -0400

    complete lab

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! Avast ye maties! Ahoy, there. We come in search of booty!"
   end
 
 end
